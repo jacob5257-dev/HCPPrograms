cb92839a73e966a3b466c78ee91b98ee3cb253ae
S26600004 <hello@jacob5257.com>
2022-09-28 10:40:02 -0700

    Some more codingbat

--- CodingBat-Strings1/src/Main.java
+++ CodingBat-Strings1/src/Main.java
@@ -32,4 +32,33 @@ public class Main {
         return str.substring(0, str.length() / 2);
     }
 
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+
+    public String comboString(String a, String b) {
+        if (a.length() > b.length()) return b + a + b;
+        else return a + b + a;
+    }
+
+    public String nonStart(String a, String b) {
+        return a.substring(1, a.length()) + b.substring(1, b.length());
+    }
+    public String left2(String str) {
+        String x = str.substring(0, 2);
+        return str.substring(2, str.length()) + x;
+    }
+    public String right2(String str) {
+        String x = str.substring(str.length() - 2, str.length());
+        return x + str.substring(0, str.length() - 2);
+    }
+    public String theEnd(String str, boolean front) {
+        if (front) return str.substring(0, 1);
+        else return str.substring(str.length() - 1, str.length());
+    }
+    public String withouEnd2(String str) {
+        if (str.length() < 2) return "";
+        return str.substring(1, str.length() - 1);
+    }
+
 }
