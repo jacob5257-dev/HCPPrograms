af6d76a62f8e07e44d83aed6b810c44e394538dd
S26600004 <hello@jacob5257.com>
2022-10-13 11:27:08 -0700

    f

--- Strings/src/Scratch.java
+++ Strings/src/Scratch.java
@@ -2,7 +2,7 @@ public class Scratch {
     public static void main(String[] args) {
         int quantity = 3;
         double value = 7.52;
-        Sting number = "one";
+        String number = "one";
 
 
     }
