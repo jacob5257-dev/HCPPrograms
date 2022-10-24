93681caac8ff1d12afdf7b76a3acda90062d2257
Jacob5257-Development <93557046+Jacob5257-Development@users.noreply.github.com>
2022-10-24 13:17:50 -0700

    Fixed temps but better

--- Unit 3/src/TemperaturesButBetter.java
+++ Unit 3/src/TemperaturesButBetter.java
@@ -1,26 +1,66 @@
+import org.json.*;
+
+import javax.swing.*;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Objects;
 
-public class TemperaturesButBetter {
+//WARNING: READ BEFORE RUNNING!!!!!!
+//This code requires the org.json dependancy to be available. 
+//To run this program, set up a project with org.json as a dependancy. 
+//Then, copy/paste this code into the editor of your choice.
+
+public class Main {
     public static void main(String[] args) {
-        String url = "https://api.weather.gov/gridpoints/PSR/158,57/forecast";
+        double lat;
+        double lon;
+        JTextField latInput = new JTextField(5);
+        JTextField lonInput = new JTextField(5);
+        JPanel myPanel = new JPanel();
+        myPanel.add(new JLabel("Longitude: "));
+        myPanel.add(latInput);
+        myPanel.add(Box.createHorizontalStrut(15));
+        myPanel.add(new JLabel("Latitude: "));
+        myPanel.add(lonInput);
+        int result = JOptionPane.showConfirmDialog(null, myPanel, "Enter current location.", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            try {
+                lat = Double.parseDouble(latInput.getText());
+                lon = Double.parseDouble(lonInput.getText());
+            }
+            catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Invalid input.");
+                return;
+            }
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Cancelled.");
+            return;
+        }
+        String url = "https://api.weather.gov/points/" + lat + "," + lon;
         String response = getResponse(url);
-        System.out.println(response);
+        JSONObject json = new JSONObject(response);
+        if (!Objects.equals(json.getString("type"), "Feature")) {
+            if (json.getInt("status") / 100 == 5) System.out.println("There was an issue with the weather data. This is not a bug, please try again later.");
+            else System.out.println(json.getString("detail"));
+            return;
+        }
+        url = json.getJSONObject("properties").getString("forecast");
 
-
-
-
-        /*
-        Goal:
-        ✅ Get the response from the API in url variable;
-        ❌ Parse response from api to json object;
-        ❌ And gather the temperature from the json object;
-
-        I've seen some people use the JSONObject from javax.json library, but that doesn't seem to be packaged with Java since Java 7 (I think).
-        Some people also use external libraries like GSON or org.json, but I'm not sure how to add it without creating a package. Also not something I particularly want to deal with.
-         */
+        response = getResponse(url);
+        json = new JSONObject(response);
+        JSONArray periods = json.getJSONObject("properties").getJSONArray("periods");
+        int temp1 = periods.getJSONObject(0).getInt("temperature");
+        int temp2 = periods.getJSONObject(1).getInt("temperature");
+        int highTemp = Math.max(temp1, temp2);
+        int lowTemp = Math.min(temp1, temp2);
+        String output = "High: " + highTemp + "°F. Low: " + lowTemp + "°F.\n";
+        if (highTemp >= 90) output += "Heat warning!\n";
+        if (lowTemp <= 32) output += "Freeze warning!\n";
+        if (highTemp - lowTemp > 40) output += "Large temperature swing!\n";
+        JOptionPane.showMessageDialog(null, output);
     }
 
     private static String getResponse(String url) {
