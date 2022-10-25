2e02538f5c36457cabcd889c2f2d91af71ee4613
ubuntu <hello@jacob5257.com>
2022-10-25 10:29:29 -0700

    PairOfNumbers started

---
+++ Unit 5/src/PairOfNumbers.java
@@ -0,0 +1,33 @@
+public class PairOfNumbers {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int difference(int a, int b) {
+        return a - b;
+    }
+
+    public static int product(int a, int b) {
+        return a * b;
+    }
+
+    public static double quotient(int a, int b) {
+        return (double) a / b;
+    }
+
+    public static int average(int a, int b) {
+        return (a + b) / 2;
+    }
+
+    public static int distance(int a, int b) {
+        return Math.abs(a - b);
+    }
+
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+}
