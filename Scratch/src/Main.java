a194deb7f4b503837f0b2076753d19351902d989
Jacob5257-Development <hello@jacob5257.com>
2022-11-29 12:33:54 -0700

    Automated my math homework

--- Scratch/src/Main.java
+++ Scratch/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        String test = "hello world " + (1 + 2);
-        System.out.println(test);
+        int[] numbers = {3, 6, 9, 12};
+
+        for (int number : numbers) {
+            System.out.println(pert(12, 0.5, number));
+        }
+    }
+
+    public static double zScore(int x, double mean, double stdDev) {
+        return (x - mean) / stdDev;
+    }
+
+    public static double pert(double p, double r, double t) {
+        return p * Math.exp(r * t);
     }
 }
