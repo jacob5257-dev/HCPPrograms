294492dd1a26838f5ea2f6826a3109e50f27b30f
Jacob5257-Development <hello@jacob5257.com>
2023-01-09 12:49:12 -0700

    Added other requirements (xum, min, max, median)

--- Unit 6/src/RandomArray.java
+++ Unit 6/src/RandomArray.java
@@ -16,5 +16,13 @@ public class RandomArray {
             reverse[i] = array[9 - i];
         }
         System.out.println(Arrays.toString(reverse));
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += array[i];
+        }
+        int min = array[0];
+        int max = reverse[0];
+        double median = (array[4] + array[5]) / 2.0;
+        System.out.println("Sum: " + sum + ", min: " + min + ", max: " + max + ", median: " + median);
     }
 }
