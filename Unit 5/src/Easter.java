46641459728dab5178ee240ec57b7831e9f16474
Jacob5257-Development <hello@jacob5257.com>
2023-01-12 10:03:39 -0700

    Fixed easter program 	modified:   Unit 5/src/Easter.java     modified:   Unit 5/src/EasterTest.java 	modified:   out/production/Unit 5/Easter.class 	modified:   out/production/Unit 5/EasterTest.class 	modified:   out/production/Unit 5/PairOfNumbers.class 	modified:   out/production/Unit 5/PairOfNumbersTest.class

--- Unit 5/src/Easter.java
+++ Unit 5/src/Easter.java
@@ -1,29 +1,36 @@
 public class Easter {
-    private int year;
+    private final int year;
     
     public Easter(int year) {
         this.year = year;
     }
 
-    int a = year % 19;
-    int b = year / 100;
-    int c = year % 100;
-    int d = b / 4;
-    int e = b % 4;
-    int g = (b * 8 + 13) / 25;
-    int h = (19 * a + b - d - g + 15) % 30;
-    int j = c / 4;
-    int k = c % 4;
-    int m = (a + 11 * h) / 319;
-    int r = (2 * e + 2 * j - k - h + m + 32) % 7;
-    int n = (h - m + r + 90) / 25;
-    int p = (h - m + r + n + 19) % 32;
+    public String getEasterDate() {
+        String result;
+        int a = year % 19,
+            b = year / 100,
+            c = year % 100,
+            d = b / 4,
+            e = b % 4,
+            g = (8 * b + 13) / 25,
+            h = (19 * a + b - d - g + 15) % 30,
+            j = c / 4,
+            k = c % 4,
+            m = (a + 11 * h) / 319,
+            r = (2 * e + 2 * j - k - h + m + 32) % 7,
+            n = (h - m + r + 90) / 25,
+            p = (h - m + r + n + 19) % 32;
+        switch(n)
+        {
+            case 2 -> result = "February ";
+            case 3 -> result = "March ";
+            case 4 -> result = "April ";
+            case 5 -> result = "May ";
+            default -> {
+                return "Error!";
+            }
+        }
 
-    public int getMonth() {
-        return n;
-    }
-
-    public int getDay() {
-        return p;
+        return result + p;
     }
 }
--- Unit 5/src/EasterTest.java
+++ Unit 5/src/EasterTest.java
@@ -2,13 +2,16 @@ import javax.swing.JOptionPane;
 
 public class EasterTest {
     public static void main(String[] args) {
-        String[] months = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         String yearSelectString = JOptionPane.showInputDialog("Enter the year: ");
-        int year = Integer.parseInt(yearSelectString);
+        int year = 0;
+        try {
+            year = Integer.parseInt(yearSelectString);
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Cancelled or invalid input.");
+            System.exit(0);
+        }
         Easter easter = new Easter(year);
-        System.out.println(year);
-        System.out.println(easter.getMonth());
-        System.out.println(easter.getDay());
-        JOptionPane.showMessageDialog(null, "Easter is on " + months[easter.getMonth()] + " " + easter.getDay() + ".");
+        JOptionPane.showMessageDialog(null, "Easter is on " + easter.getEasterDate() + ".");
     }
 }
