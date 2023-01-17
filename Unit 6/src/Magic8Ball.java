71c1c612b53c5d18f07d5fa84088ceb2a6e67f50
Jacob5257-Development <hello@jacob5257.com>
2023-01-17 12:49:20 -0700

    Fixed wrong character in magic 8 ball 	modified:   Unit 6/src/Magic8Ball.java

--- Unit 6/src/Magic8Ball.java
+++ Unit 6/src/Magic8Ball.java
@@ -13,7 +13,7 @@ public class Magic8Ball {
     public String getResponse() {
         Random random = new Random();
         //List of potential responses by the 8 ball
-        String[] responses = {"It is certain", "Reply hazy, try again", "Don’t count on it", "It is decidedly so", "Ask again later", "My reply is no", "Without a doubt", "Better not tell you now", "My sources say no", "Yes definitely", "Cannot predict now", "Outlook not so good", "You may rely on it", "Concentrate and ask again", "Very doubtful", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes"};
+        String[] responses = {"It is certain", "Reply hazy, try again", "Don't count on it", "It is decidedly so", "Ask again later", "My reply is no", "Without a doubt", "Better not tell you now", "My sources say no", "Yes definitely", "Cannot predict now", "Outlook not so good", "You may rely on it", "Concentrate and ask again", "Very doubtful", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes"};
         return responses[random.nextInt(responses.length)];
     }
 }
