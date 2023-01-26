c9f6f4dc67d6c3ef9b303b37ce206e028255b6e9
Jacob5257-Development <hello@jacob5257.com>
2023-01-26 13:32:33 -0700

    added loooooooooooop

--- Unit 5/src/VendingMachineTest.java
+++ Unit 5/src/VendingMachineTest.java
@@ -8,26 +8,30 @@ public class VendingMachineTest {
         System.out.print("Enter the price of a can: ");
         double price = scanner.nextDouble();
         VendingMachine machine = new VendingMachine(price);
-        //machine.fillUp(10);
-        JOptionPane optionPane = new JOptionPane("Do you want to buy a soda can?", JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION);
-        JDialog dialog = optionPane.createDialog(null, "Vending Machine");
-        dialog.setVisible(true);
-        int input = (int) optionPane.getValue();
-        if (input == JOptionPane.YES_OPTION && machine.getCanCount() > 0) {
-            double moneyInserted = 0;
-            while (moneyInserted < price) {
-                String moneyInput = JOptionPane.showInputDialog(null, "Money inserted: " + moneyInserted + " Enter a coin: ");
-                machine.insertCoin(Double.parseDouble(moneyInput));
-                moneyInserted += Double.parseDouble(moneyInput);
-            }
-            JOptionPane.showMessageDialog(null, "Enjoy your soda can! There are " + machine.getCanCount() + " cans left in the machine. The machine has $" + machine.getBankBalance() + " in it.");
+        machine.fillUp(10);
+        while (true) { 
+            JOptionPane optionPane = new JOptionPane("Do you want to buy a soda can?", JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION);
+            JDialog dialog = optionPane.createDialog(null, "Vending Machine");
+            dialog.setVisible(true);
+            int input = (int) optionPane.getValue();
+            if (input == JOptionPane.YES_OPTION && machine.getCanCount() > 0) {
+                double moneyInserted = 0;
+                while (moneyInserted < price) {
+                    String moneyInput = JOptionPane.showInputDialog(null, "Money inserted: " + moneyInserted + " Enter a coin: ");
+                    machine.insertCoin(Double.parseDouble(moneyInput));
+                    moneyInserted += Double.parseDouble(moneyInput);
+                }
+                JOptionPane.showMessageDialog(null, "Enjoy your soda can! There are " + machine.getCanCount() + " cans left in the machine. The machine has $" + machine.getBankBalance() + " in it.");
 
-        }
-        else if (input == JOptionPane.NO_OPTION) {
-            JOptionPane.showMessageDialog(null, "There are " + machine.getCanCount() + " cans left in the machine. The machine has $" + machine.getBankBalance() + " in it.");
-        }
-        else {
-            JOptionPane.showMessageDialog(null, "The machine is out of soda cans.");
+            }
+            else if (input == JOptionPane.NO_OPTION) {
+                JOptionPane.showMessageDialog(null, "There are " + machine.getCanCount() + " cans left in the machine. The machine has $" + machine.getBankBalance() + " in it.");
+                break;
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "The machine is out of soda cans.");
+                break;
+            }
         }
         scanner.close();
         System.exit(0);
