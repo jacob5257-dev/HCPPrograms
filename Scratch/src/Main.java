7314eef6bb24ce145f0798eb8f5e66105d64a9a2
Jacob5257-Development <hello@jacob5257.com>
2023-02-03 12:30:05 -0700

    h

--- Scratch/src/Main.java
+++ Scratch/src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
+    private Object storage;
+    public Main(Object storage) {
+        this.storage = storage;
+    }
 
+    public Object getStorage() {
+        return storage;
+    }
 
+    public void setStorage(Object storage) {
+        this.storage = storage;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main(1);
+        System.out.println(main.getStorage());
+    }
 }
