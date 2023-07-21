7cea8bb0470b87eabbcdcfe34fca9df3c4f86943
jacob <78556094+jacob52571@users.noreply.github.com>
2023-07-21 10:25:03 -0700

    Actually add the option to split the bill and added comments

--- Unit 6.5/src/TipCalculator.java
+++ Unit 6.5/src/TipCalculator.java
@@ -1,11 +1,20 @@
+// Important imports
 import javax.swing.*;
 import java.text.DecimalFormat;
 
-public class Main {
+/**
+ * This program will calculate the tip amount and total bill based on the original bill amount and tip percentage.
+ * @version 7/21/2023
+ * @author jacob5257
+ * you're welcome greg
+ */
+public class TipCalculator {
     public static void main(String[] args) {
         // Create a program that will take in a bill amount and a tip percentage and calculate the dollar amount of tips and the total bill
-        String input = JOptionPane.showInputDialog("How much money was the original bill? (in dollars)");
-        double originalBill = Double.parseDouble(input);
+        double originalBill = 0;
+        String input;
+        try { originalBill = Double.parseDouble(JOptionPane.showInputDialog("How much money was the original bill? (in dollars)")); }
+        catch (Exception e) { JOptionPane.showMessageDialog(null, "Invalid input. Please enter a number."); System.exit(0); }
         // Give the user the option to tip 15%, 18%, 20%, or custom. If they select custom, ask them for the tip percentage
         Object[] options = {"15% (okay)", "18% (good)", "20% (great)", "Custom"};
         JPanel panel = new JPanel();
@@ -16,17 +25,34 @@ public class Main {
         } else {
             input = options[selection].toString().substring(0, 2);
         }
+        // Convert tip percentage to a double, throw an error if the input is invalid
         double tipPercentage = 0;
         try { tipPercentage = Double.parseDouble(input); }
         catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Invalid input. Please enter a number.");
             System.exit(0);
         }
-        // Round the tip amount to the nearest cent
+        // Round the tip amount to the nearest cent and calculate the total bill
         DecimalFormat df = new DecimalFormat("#.##");
         double tipAmount = Double.parseDouble(df.format(originalBill * (tipPercentage / 100)));
         double totalBill = originalBill + tipAmount;
-        JOptionPane.showMessageDialog(null, "The tip amount is $" + tipAmount + " and the total bill is $" + totalBill);
+        // Give the user the option to split the bill
+        JPanel panel2 = new JPanel();
+        Object[] options2 = {"Yes", "No"};
+        panel2.add(new JLabel("Do you want to split the bill?"));
+        int selection2 = ( JOptionPane.showOptionDialog(null, panel2, "Select an option.", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options2, null));
+        // If they want to split the bill, ask them how many people are splitting the bill and calculate the split bill. Otherwise, just display the tip amount and total bill.
+        if (selection2 == 0) {
+            input = JOptionPane.showInputDialog("How many people are splitting the bill?");
+            int people = 0;
+            try { people = Integer.parseInt(input); }
+            catch (Exception e) { JOptionPane.showMessageDialog(null, "Invalid input. Please enter a number."); System.exit(0); }
+            double splitBill = totalBill / people;
+            JOptionPane.showMessageDialog(null, "The total tip is $" + tipAmount + " and the total bill is $" + totalBill + ". The split bill is $" + splitBill + " per person.");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "The total tip is $" + tipAmount + " and the total bill is $" + totalBill + ".");
+        }
 
     }
 }
