829b4a7554649ab4d51f13a9148f20899d033043
jacob <78556094+jacob52571@users.noreply.github.com>
2023-07-25 15:13:22 -0700

    updated scratch

--- Scratch/src/Main.java
+++ Scratch/src/Main.java
@@ -1,7 +1,5 @@
-import java.util.List;
-import java.util.ArrayList;
-
 public class Main {
+    // Automated my math hw lol
     public static void main(String[] args) {
         System.out.println(math3(5.0, Math.sqrt(5.0)));
     }
