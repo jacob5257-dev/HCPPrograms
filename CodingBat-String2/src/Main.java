040c5a8db4ed90014c96d23ef78bb14d47c672ec
jacob <78556094+jacob52571@users.noreply.github.com>
2023-07-26 13:44:24 -0700

    added new codingbat

--- CodingBat-String2/src/Main.java
+++ CodingBat-String2/src/Main.java
@@ -8,4 +8,11 @@ public class Main {
         return return_str;
     }
 
+    public int countHi(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.startsWith("hi", i)) count++;
+        }
+        return count;
+    }
 }
