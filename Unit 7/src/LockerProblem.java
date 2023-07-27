e73cbc3a7be4edd5e7ed2dfbbb7b5df119945da6
jacob <78556094+jacob52571@users.noreply.github.com>
2023-07-27 15:09:11 -0700

    locker problem added

---
+++ Unit 7/src/LockerProblem.java
@@ -0,0 +1,21 @@
+public class LockerProblem {
+    public static void main(String[] args) {
+        // Create an array of 100 booleans representing each locker.
+        boolean[] lockers = new boolean[100];
+        // Loop through the array, with i being the student
+        for (int i = 0; i < 100; i++) {
+            // Loop through the array again, with j being the locker. When they match, it's a multiple of i + 1. i + 1 is used because the first locker is 0 and otherwise it won't increment the first time.
+            for (int j = i; j < 100; j += i + 1) {
+                // If the locker is open, close it. If it's closed, open it.
+                lockers[j] = !lockers[j];
+            }
+        }
+        // Loop through the array one last time, printing the locker number if it's open
+        for (int i = 0; i < 100; i++) {
+            if (lockers[i]) {
+                System.out.println(i + 1);
+            }
+        }
+    }
+
+}
