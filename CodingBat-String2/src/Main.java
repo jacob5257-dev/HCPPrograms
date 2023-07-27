e938f4ff115d763cb2855c3713b5db74c91a44b1
jacob <78556094+jacob52571@users.noreply.github.com>
2023-07-27 14:36:57 -0700

    added catdog codingbat

--- CodingBat-String2/src/Main.java
+++ CodingBat-String2/src/Main.java
@@ -15,4 +15,14 @@ public class Main {
         }
         return count;
     }
+
+    public boolean catDog(String str) {
+        int cat_count = 0;
+        int dog_count = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.startsWith("cat", i)) cat_count++;
+            if (str.startsWith("dog", i)) dog_count++;
+        }
+        return cat_count == dog_count;
+    }
 }
