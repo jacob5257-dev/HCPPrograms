4de9a7546931e9aa928a4882ff7b070ce108572e
jacob <78556094+jacob52571@users.noreply.github.com>
2023-07-28 14:19:36 -0700

    repeatend codingbat

--- CodingBat-String2/src/Main.java
+++ CodingBat-String2/src/Main.java
@@ -91,4 +91,12 @@ public class Main {
         }
         return str;
     }
+
+    public String repeatEnd(String str, int n) {
+        String string = "";
+        for (int i = 0; i < n; i++) {
+            string += str.substring(str.length() - n);
+        }
+        return string;
+    }
 }
