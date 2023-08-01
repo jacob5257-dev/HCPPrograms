c08253ae5185dfbe2ae5d1d78687ff707334a9c4
jacob <78556094+jacob52571@users.noreply.github.com>
2023-08-01 08:51:31 -0700

    tried to find answer to prefixagain codingbat

--- Scratch/src/Main.java
+++ Scratch/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     // Automated my math hw lol
+    /*
     public static void main(String[] args) {
         System.out.println(math3(5.0, Math.sqrt(5.0)));
     }
@@ -26,4 +27,20 @@ public class Main {
         }
         return (qy - py) / (qx - px);
     }
+     */
+
+    public static void main(String[] args) {
+        System.out.println(prefixAgain("abXYabc", 1));
+        System.out.println(prefixAgain("abXYabc", 2));
+    }
+
+    public static boolean prefixAgain(String str, int n) {
+        String substring = str.substring(0, n);
+        for (int i = 0; i < str.length() - substring.length() - 1; i++) {
+            String abc = str.substring(n + i - 1, n + i + substring.length());
+            if (abc.equals(substring)) return true;
+        }
+        return false;
+    }
+
 }
