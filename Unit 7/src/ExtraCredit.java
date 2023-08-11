1c758f50b2c7b6e076c71391754a460593716f4a
jacob <lijacob290@gmail.com>
2023-08-11 14:49:01 -0700

    extra credit done

---
+++ Unit 7/src/ExtraCredit.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ExtraCredit {
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        java.awt.Desktop.getDesktop().browse(new URI("https://bit.ly/3BlS71b"));
+    }
+}
