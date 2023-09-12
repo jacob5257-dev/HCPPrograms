83b35a2e56ed95cf23ca15894a60ee87f95b2b30
jacob <lijacob290@gmail.com>
2023-09-12 09:32:15 -0700

    proper comments in dividestrings

--- Scratch/src/Main.java
+++ Scratch/src/Main.java
@@ -1,27 +1,10 @@
 //import java.util.ArrayList;
 //import java.util.List;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Main m = new Main();
-        m.changer(m.s, m.n);
-        System.out.println(m);
-    }
-
-    private String s;
-    private int n;
-
-    public Main() {
-        this.s = "world";
-        this.n = 6;
-    }
-
-    public void changer(String x, int y) {
-        x = x + "peace";
-        y = y * 2;
-    }
-
-    public String toString() {
-        return s + " " + n;
+        ArrayList<ArrayList<String>> epicList = new ArrayList<>();
     }
 }
--- Unit 7/src/DivideStrings.java
+++ Unit 7/src/DivideStrings.java
@@ -3,29 +3,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DivideStrings {
-    /*
-    Write an application that accepts up to 20 strings.  Divide them into two array lists - one for short Strings that are five characters or fewer, and the other for long Strings.  After data entry is complete, prompt the user to enter which type of String to display, and then output to the correct list.  If there are no Strings in a requested list, then output an appropriate message.  Save the file as DivideStrings.java
+    /**
+     * This program will ask the user for strings and then divide them into short and long strings.
+     * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Create an arraylist for the user to put strings into
         List<String> strings = new ArrayList<>();
+        // Iterate 20 times and ask the user for a string each time. If the user enters nothing, then stop asking for strings. (max 20 times)
         for (int i = 0; i < 20; i++) {
+            // JOption cuz why not
             String string = JOptionPane.showInputDialog("Enter a string: ");
+            // If they leave the string blank or hit cancel, stop asking for strings
             if (string == null || string.isEmpty()) {
                 break;
             }
+            // Add input to the arraylist
             strings.add(string);
         }
+        // Create new arraylists for short and long strings
         List<String> shortStrings = new ArrayList<>();
         List<String> longStrings = new ArrayList<>();
+        // Check through each string, and add it to shortStrings if it's less than 5 characters, or add it to longStrings if it's more than 5 characters
         for (String str : strings) {
             if (str.length() > 5) longStrings.add(str);
             else shortStrings.add(str);
         }
+        // Ask the user which list they want
         Object[] options = {"Short strings", "Long strings"};
         JPanel panel = new JPanel();
         panel.add(new JLabel("Which strings would you like to display?"));
         int selection = ( JOptionPane.showOptionDialog(null, panel, "Select an option.", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null));
         String output = "";
+        // If the user selects short, then print all short strings, and vice versa with long
         if (selection == 0) {
             for (String str : shortStrings) {
                 output += str;
@@ -38,10 +48,12 @@ public class DivideStrings {
                 output += "\n";
             }
         }
+        // If something breaks and selection isn't right, error ig
         else {
             JOptionPane.showMessageDialog(null, "You didn't select an option.");
             System.exit(0);
         }
+        // Second ternary = short or long (name of list), first ternary = handling if list is empty
         JOptionPane.showMessageDialog(null, (output.isEmpty() ? "There are no " + (selection == 0 ? "short" : "long") + " strings." : output));
     }
 }
