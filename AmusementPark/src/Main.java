205d1194faa053fff0544c8f19a5bbd2e8924d82
jacob <lijacob290@gmail.com>
2023-10-25 13:58:29 -0700

    amusementpark started

---
+++ AmusementPark/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Used only to run other code (use other files for stuff)
+        System.out.println("Hello world!");
+    }
+}
--- Scratch/src/Main.java
+++ Scratch/src/Main.java
@@ -2,7 +2,25 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt(6) + " " + random.nextInt(6));
+        SubClass a = new SubClass("a");
+    }
+}
+
+class SuperClass {
+    private String name;
+
+    public SuperClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class SubClass extends SuperClass {
+    private String type;
+    public SubClass(String name) {
+        type = name;
     }
 }
