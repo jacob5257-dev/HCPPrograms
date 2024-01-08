95ad46cad84fd7ce44078516021f7c9c1f3beef9
jacob <lijacob290@gmail.com>
2024-01-08 15:33:30 -0700

    testing something

--- Scratch/src/Main.java
+++ Scratch/src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print(d( 2, 8));
-    }
-
-    public static double d(int x, int m) {
-        System.out.println("In power " + x + " : " + m);
-        if (m == 0) return 1.0;
-        else if (m % 2 != 0) return x * Math.pow(d(x, m / 2), 2);
-        else return Math.pow(d(x, m / 2), 2);
+        System.out.print("hello world");
     }
 }
