2a449ca81adbcbf6ee2aad9392d37026c2e3eefe
jacob <lijacob290@gmail.com>
2024-02-01 10:34:18 -0700

    added infinite gameplay and win/loss counter

--- Scratch/src/Main.java
+++ Scratch/src/Main.java
@@ -4,6 +4,29 @@ import java.io.FileNotFoundException;
 // this code is messy but it works
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        int wins = 0;
+        int losses = 0;
+        while (true) {
+            if (play()) {
+                wins++;
+            } else {
+                losses++;
+            }
+            System.out.println("Wins: " + wins + " Losses: " + losses);
+            if (JOptionPane.showConfirmDialog(null, "Play again?") != 0) {
+                break;
+            }
+        }
+    }
+
+    public static boolean hasDashes(char[] dashes) {
+        for (char c : dashes) {
+            if (c == '-') return true;
+        }
+        return false;
+    }
+
+    public static boolean play() throws FileNotFoundException {
         Hangman hangman = new Hangman();
         System.out.println("The category is: " + hangman.getCategory());
         while (hangman.getValidGuesses() != 0) {
@@ -48,15 +71,10 @@ public class Main {
 
         if (hangman.getGuessed()) {
             System.out.println("You win!");
+            return true;
         } else {
             System.out.println("You lose! The word was " + hangman.getSecretWord());
+            return false;
         }
-    }
-
-    public static boolean hasDashes(char[] dashes) {
-        for (char c : dashes) {
-            if (c == '-') return true;
-        }
-        return false;
     }
 }
