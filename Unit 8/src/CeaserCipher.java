dd7ca521ca59f9977ab6aa8d29b7a5c920d1cfc0
jacob52571 <lijacob290@gmail.com>
2024-02-26 15:05:31 -0700

    Add files via upload
    Signed-off-by: jacob52571 <lijacob290@gmail.com>

--- Unit 8/src/CeaserCipher.java
+++ Unit 8/src/CeaserCipher.java
@@ -1,16 +1,16 @@
 import javax.swing.*;
 
 public class CeaserCipher {
-public static void main(String[] args) {
-String original = JOptionPane.showInputDialog("Decrypted message: ").toUpperCase();
-int n = Integer.parseInt(JOptionPane.showInputDialog("Offset: "));
-char[] chars = original.toCharArray()
-String encrypted = "";
-for (char c : chars) {
-c += n;
-if (c > 90) c -= 26;
-encrypted += c;
-}
-JOptionPane.showMessageDialog(null, 
-}
+	public static void main(String[] args) {
+		String original = JOptionPane.showInputDialog("Decrypted message: ").toUpperCase();
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Offset: "));
+		char[] chars = original.toCharArray();
+		String encrypted = "";
+		for (char c : chars) {
+			c += n;
+			if (c > 90) c -= 26;
+			encrypted += c;
+		}
+		JOptionPane.showMessageDialog(null, encrypted);
+	}
 }
